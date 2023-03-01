9a5a95b6ecbf6e8feae09d121c9e89425b84da01
Anastasia Vasileva <ayavasilek@gmail.com>
2023-03-01 15:38:36 +0300

    Search longest word

---
+++ Search_longest_word.py
@@ -0,0 +1,7 @@
+string = input("Введите что-нибудь: ")
+words = string.split()
+longest_word = ''
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+print(longest_word)
